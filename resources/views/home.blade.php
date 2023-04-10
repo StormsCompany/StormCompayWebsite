9e96fe3a9360f52f51ac22e8bc7ea309cd28a298
kiplangatsang <kiplangatsang425@gmail.com>
2023-04-10 10:24:43 +0300

    home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,15 +9,18 @@
 								</div>
 								<div class="col-md-8 col-xl-7 mx-auto">
 												<div class="item">
-																<label class="control-label">You have made a request</label>
+																<div class="d-flex justify-content-between">
+																				<h3 class="control-label mx-auto">Have you made a request?</h3>
+																</div>
 																<br>
 																<div class="row justify-content-center">
-																				<a href="{{ route('services.index') }}" class="btn col-md-3 col-xl-3 btn-primary p-3 m-3 rounded-pill ">
+																				<a href="{{ route('services.index') }}"
+																								class="btn btn-sm col-md-3 col-xl-3 btn-primary p-2 m-3 rounded-pill ">
 																								View requests <i class="fa fa-eye  mx-auto"></i>
 																				</a>
 																				<a href="{{ route('services.create') }}"
-																								class="btn col-md-3 col-xl-3 btn-secondary p-3 m-3 rounded-pill ">
-																								Add another <i class="fa fa-eye  mx-auto"></i>
+																								class="btn btn-sm col-md-3 col-xl-3 btn-secondary p-2 m-3 rounded-pill ">
+																								Add one now... <i class="fa fa-plus  mx-auto"></i>
 																				</a>
 																</div>
 																<br>
